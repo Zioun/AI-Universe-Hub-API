9cf9e36d761087c18bfc6e77d1063c44a462feb6
Zioun <jionkhan0@gmail.com>
2024-03-01 22:35:58 +0600

    added

--- js/xyz.js
+++
@@ -1,5 +0,0 @@
-const shortDataBtn = document.getElementById("show-all-button");
-    shortDataBtn.classList.remove("hidden");
-
-    const showAllButton = document.getElementById("show-all-button");
-    showAllButton.classList.add("hidden");
